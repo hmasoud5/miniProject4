d02d6f908d84ff88cd85cb15c6006673a634ff9e
Heba Masoud <heba.masoud@patelhs.org>
2021-01-29 13:08:03 +0000

    added boolean method

--- Main.java
+++ Main.java
@@ -1,8 +1,23 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 class Main {
    public static void main(String[] args) {
     
     Scanner scan = new Scanner(System.in);
+
+
+
   }
-}
+
+  static int 
+
+  static boolean passClass ( int testGrade )
+  {
+    if ( testGrade > 70 )
+    {
+     return true;
+    }else{
+      return false; 
+    }
+  }  
+  }
