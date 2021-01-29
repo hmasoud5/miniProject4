96ac51b5b85b67de4097ea418be395d998dd5fc4
Heba Masoud <heba.masoud@patelhs.org>
2021-01-29 13:55:01 +0000

    added print statement to add credits

--- Main.java
+++ Main.java
@@ -4,28 +4,29 @@ class Main {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-    
+    // recieving user input from the user
+
     System.out.println(" Enter in your grade : ");
-    int grade = scan.next();
-    
-    int grade = totalGrade; 
-    if (passClass(grade))
-    {
-     System.out.println(" Three credits will be added! ");
-     (credits(totalGrade))
-    }else{
+    // enter in your grade in order to find out if you recieve credits
+    int grade = scan.nextInt();
+
+    int totalGrade = 0;
+    if (passClass(grade)) {
+      System.out.println(" Three credits will be added! ");
+      grade = credits(totalGrade);
+      System.out.println(" Your total credits now " + grade);
+    } else {
       System.out.println(" Must retake class. ");
     }
-
 
   }
 
   // method calculates credits
   static int credits(int totalGrade) {
-    if (passed) {
-      totalGrade = totalGrade + 3;
-      // if passed class then three credits will be added
-    }
+
+    totalGrade = totalGrade + 3;
+    // if passed class then three credits will be added
+
     return totalGrade;
   }
 
