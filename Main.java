73fd9e4d4a1ec1c0bfcc151ead7695f5fe05bf5c
Heba Masoud <heba.masoud@patelhs.org>
2021-01-29 13:23:57 +0000

    added three comments for guiding

--- Main.java
+++ Main.java
@@ -4,25 +4,31 @@ class Main {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
+     
+
+
 
 
 
   }
-
+   //method calculates credits 
   static int credits (int passed , int totalGrade)
   {
     if ( passed )
     {
      totalGrade = totalGrade + 3; 
+     //if passed class then three credits will be added
     }
     return totalGrade; 
   }
-
+   //method calculates passing class
   static boolean passClass(int grade) {
     if (grade > 70) {
       return true;
+      //return true if your grade is greater than 70 ; meaning you passed the class.
     } else {
       return false;
+      //return false if your grade is less than 70 ; meaning you passed the class. 
     }
   }
 }
