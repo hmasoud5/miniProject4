3b85081a6837887826282cb6d8360430035a64c4
Heba Masoud <heba.masoud@patelhs.org>
2021-02-01 18:42:05 +0000

    working code, ready to submit!

--- Main.java
+++ Main.java
@@ -5,22 +5,29 @@ class Main {
 
     Scanner scan = new Scanner(System.in);
     // recieving user input from the user
-
-    System.out.println(" Enter in your grade: ");
-    // enter in your grade in order to find out if you recieve credits
-    int grade = scan.nextInt();
-
+    System.out.println("How many classes do you have?"); 
+    int classes = scan.nextInt();
+    
     int totalCredits = 0;
-    if (passClass(grade)) {
+
+    for(int i = 0 ; i < classes ; i++)
+    {
+      System.out.println("What is your class name?");
+      String subject = scan.next();
+      // enter in your grade in order to find out if you recieve credits
+      System.out.println(" Enter in your grade: ");
+      int grade = scan.nextInt();
+
+      if (passClass(grade)) {
       System.out.println(" Three credits will be added! ");
       totalCredits = credits(totalCredits);
       System.out.println(" Your total credits now : " + totalCredits);
-    } else {
-      System.out.println(" Must retake class. ");
+      } else {
+        System.out.println(" Must retake class. ");
 
-      System.out.println(" Total credits now : " + totalCredits);
+        System.out.println(" Total credits now : " + totalCredits);
+      } 
     }
-
   }
 
   // method calculates credits
